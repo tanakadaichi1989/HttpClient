ace9e2fb57d7d5c79832972ff12933df2e27544f
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-07-14 23:26:58 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri  url = new Uri("https://zipcloud.ibsnet.co.jp/api/search?zipcode=1000000");
+            Task<String> webTask = SampleHttpClient.GetAsync(url);
+            webTask.Wait();
+            String result = webTask.Result;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ SampleHttpClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    public class SampleHttpClient
+    {
+        public static async Task<String> GetAsync( Uri  url)
+        {
+            using ( HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetStringAsync(url);
+                    return response;
+                } catch (HttpRequestException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
